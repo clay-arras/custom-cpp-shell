39198103f4de9536a6f8459ad6e055bde71c1ff5
Neil Lin <neillin164@gmail.com>
2024-12-07 16:26:02 -0800

    third

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,20 @@
 #include <unordered_map>
 #include <functional>
 
-// std::unordered_map<std::string, std::function<std::string(bool)>> function_map;
+std::unordered_map<std::string, std::function<std::string(bool)>> function_map;
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  while (true) {
+    std::cout << "$ ";
   
-  std::string input;
-  std::getline(std::cin, input);
+    std::string input;
+    std::getline(std::cin, input);
 
-  // if (!function_map.contains(input)) {
-  std::cout << input << ": command not found\n";
-  // }
+    if (!function_map.contains(input)) {
+      std::cout << input << ": command not found\n";
+    }
+  }
 }
