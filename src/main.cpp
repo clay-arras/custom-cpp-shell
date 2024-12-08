48c0db0118edd4aa0d727a8286d3b0062f5c95e5
Neil Lin <neillin164@gmail.com>
2024-12-07 16:17:28 -0800

    second step

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <functional>
+
+std::unordered_map<std::string, std::function<std::string(bool)>> function_map;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -10,4 +16,8 @@ int main() {
   
   std::string input;
   std::getline(std::cin, input);
+
+  if (!function_map.contains(input)) {
+    std::cout << input << ": not found\n";
+  }
 }
